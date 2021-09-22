215f18bfc6327c7a5a5086e55b2d8516654b978b
vladikshkwok <vladikshkwok@gmail.com>
2021-09-22 15:20:00 +0300

    pew pew

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -4,11 +4,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Server {
     private static Gson gson;
     String dst;
+    String test;
     String [] gateways;
 
 
@@ -31,23 +34,56 @@ public class Server {
         return channels;
     }
 
-    public void  setRouteToServer(ArrayList<Channel> routes) {
-        boolean needRoute = false;
+//    public void  setRouteToServer(ArrayList<Channel> routes) {
+//        for (String gw: gateways) {
+//            for (Channel route : routes) {
+//                if (gw.equals(route.gateway) && route.isDstConnected) {
+//                    List<Channel> thereSameServers = routes.stream().filter(u -> u.dst.equals(dst)).collect(Collectors.toList());
+//                    if (thereSameServers.size() > 0) {
+//                        for (Channel serv: thereSameServers) {
+//                            if (gw.equals(serv.gateway)) {
+//                                System.out.println("Сервер " + serv.dst + " через шлюз " + serv.gateway + " уже есть, не удаляю");
+//                                return;
+//                            }
+//                            else {
+//                                System.out.println("Удаляю сервер " + serv.dst + ", прописанный через шлюз " + serv.gateway);
+//                                return;
+//                            }
+//                        }
+//                    } else {
+//                        System.out.println("Создаю роут до сервера " + dst + " через шлюз " + gw);
+//                        return;
+//                    }
+//                } else {
+//                    System.out.println("Skip " + dst + " through " + gw);
+//                }
+//            }
+//        }
+//
+//    }
+
+
+    public void  setRouteToServer(List<Channel> routes) {
+        List<Channel> thereSameServers = routes.stream().filter(u -> u.dst.equals(dst)).collect(Collectors.toList());
         for (String gw: gateways) {
-            for (Channel route : routes) {
-                if (gw.equals(route.gateway) && route.isDstConnected) {
-                    needRoute = true;
-                } else {
-                    System.out.println("Skip " + dst + " through " + gw);
+            boolean thereSameServerRoute = routes.stream().anyMatch(u -> u.dst.equals(dst) && u.gateway.equals(gw));
+            if (thereSameServerRoute)
+                return;
+            else {
+                if () //vladik gay
+                for (Channel serv: thereSameServers) {
+
                 }
-                if (gw.equals(route.gateway) && dst.equals(route.dst))
-                    needRoute = false;
+                for (Channel route: routes) {
+                    if (gw.equals(route.gateway) && route.isDstConnected && !route.dst.equals(dst)) {
+
+                    }
+                }
             }
         }
-        if (needRoute) {
-            System.out.println(dst + " --- Нужен маршрут");
-        }
     }
+
+
 
 
     @Override
@@ -59,5 +95,14 @@ public class Server {
         return "Server{" +
                 "dst='" + dst + ", '" +
                 gatewayString + "}";
+    }
+
+
+    public static Channel findGateway (ArrayList<Channel> routes, String gateway) {
+        for (Channel chan: routes) {
+            if (chan.gateway.equals(gateway)) ;
+            return chan;
+        }
+        return null;
     }
 }
